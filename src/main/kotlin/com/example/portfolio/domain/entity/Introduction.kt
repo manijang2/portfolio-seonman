7b8785860acb4e8251188483868323519b667d28
manijang2 <manijang2@naver.com>
2025-03-06 19:38:20 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/example/portfolio/domain/entity/Introduction.kt
+++ src/main/kotlin/com/example/portfolio/domain/entity/Introduction.kt
@@ -3,12 +3,19 @@ package com.example.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Introduction : BaseEntity() {
+class Introduction(
+    content: String,
+    isActive: Boolean,
+
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "introduction_id")
     var id: Long? = null
+
+    var content: String = content
+    var isActive: Boolean = isActive
 }
 
 
