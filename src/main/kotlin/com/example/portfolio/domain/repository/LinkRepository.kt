6cf32689b8284f1d22474a2e0a700d7469741b92
manijang2 <manijang2@naver.com>
2025-03-07 20:35:25 +0900

    리포지토리 개발

--- src/main/kotlin/com/example/portfolio/domain/repository/LinkRepository.kt
+++ src/main/kotlin/com/example/portfolio/domain/repository/LinkRepository.kt
@@ -3,4 +3,6 @@ package com.example.portfolio.domain.repository
 import com.example.portfolio.domain.entity.Link
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LinkRepository : JpaRepository<Link, Long>
+interface LinkRepository : JpaRepository<Link, Long> {
+    fun findAllByIsActive(isActive: Boolean): List<LinkRepository>
+}
